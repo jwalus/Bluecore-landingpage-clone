80d357f11a5686be359bc1f2288ec9c61627d681
jwalus <walusjacob@gmail.com>
2023-06-01 21:03:07 -0500

    fixed blue box text sizing

--- src/components/Campaigns.jsx
+++ src/components/Campaigns.jsx
@@ -8,26 +8,24 @@ const Campaigns = () => {
             most powerful dataset in<br /> retail.
           </span>
           </h1>
-
-        <div className="bg-dblue w-full rounded-2xl flex justify-between xl:flex-row sm:flex-col py-[80px] mt-20">
+        <div className="bg-dblue w-full rounded-2xl flex justify-between xl:flex-row sm:flex-col py-[70px] mt-20">
           <div className="pl-12">
-            <h1 className="text-golden">5 <span className="text-golden">billion</span></h1>
+            <h1 className="text-golden text-[40px] font-extrabold">5 <span className="text-golden text-[24px]">billion</span></h1>
             <p className="text-white">shoppers identified</p>
           </div>
           <div>
-            <h1 className="text-golden">300 <span className="text-golden">billion</span></h1>
+            <h1 className="text-golden text-[40px] font-extrabold">300 <span className="text-golden text-[24px]">billion</span></h1>
             <p className="text-white">behaviors processed</p>
           </div>
           <div>
-            <h1 className="text-golden">500 <span className="text-golden">billion</span></h1>
+            <h1 className="text-golden text-[40px] font-extrabold">500 <span className="text-golden text-[24px]">billion</span></h1>
             <p className="text-white">products cataloged</p>
           </div>
           <div className="pr-12">
-            <h1 className="text-golden">$11 <span className="text-golden">billion+</span></h1>
+            <h1 className="text-golden text-[40px] font-extrabold">$11 <span className="text-golden text-[24px]">billion+</span></h1>
             <p className="text-white">revune driven</p>
           </div>
         </div>
-
       </div>
     </div>
   )
